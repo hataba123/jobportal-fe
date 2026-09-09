7a7ef68f433766941549d300bb6b95838b0ce941
hataba123 <nguyentanloi0000@gmail.com>
2026-09-09 12:27:08 +0700

    chore: patch nextjs security dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,13 @@ import "./globals.css";
 // app/page.tsx
 // // tắt Static Generation (SSG) - HTML tạo trước, khi next build
 // export const dynamic = "force-dynamic";
-export default async function RootLayout({
+export default function RootLayout({
   children,
-  params,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-  params: Promise<{ locale: string }>; // <-- Là Promise
-}) {
-  // Ensure that the incoming `locale` is valid
-  const { locale } = await params;
-  // if (!hasLocale(routing.locales, locale)) {
-  //   notFound();
-  // }
+}>) {
   return (
-    <html lang={locale}>
+    <html lang="en">
       <body className="min-h-screen flex flex-col">{children}</body>
     </html>
   );
