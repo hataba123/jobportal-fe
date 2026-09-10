270807263aa9ba8b11209fac9df5a8617fedb440
hataba123 <nguyentanloi0000@gmail.com>
2026-09-10 09:26:12 +0700

    chore: silence intentional external image fallback warning

--- src/components/common/SafeImage.tsx
+++ src/components/common/SafeImage.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+/* External images intentionally bypass next/image hostname allow-list. */
+/* eslint-disable @next/next/no-img-element */
+
 import Image from "next/image";
 import { useState } from "react";
 import { isExternalImageUrl, toBackendUrl } from "@/lib/api/url";
